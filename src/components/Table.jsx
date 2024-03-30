ff3d5f0d10569919e354b74bf8173b6820809cc1
Abhishek k <bobbyyyyy.16@gmail.com>
2024-03-30 12:02:12 +0530

    table rows alignment

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -15,22 +15,22 @@ export default function Table() {
         <thead>
           <tr className="">
             <th className="px-4 py-2">Tracking ID</th>
-            <th className="px-4 py-2">Product</th>
-            <th className="px-4 py-2">
+            <th className="px-4 py-2 text-start">Product</th>
+            <th className="px-4 py-2 text-start">
               <FaSort className="inline-block mr-1" />
               Customer
             </th>
-            <th className="px-4 py-2">
+            <th className="px-4 py-2 text-start">
               <FaSort className="inline-block mr-1" />
               Date
             </th>
-            <th className="px-4 py-2">
+            <th className="px-4 py-2 text-start">
               <FaSort className="inline-block mr-1" />
               Amount
             </th>
-            <th className="px-4 py-2">Payment Mode</th>
-            <th className="px-4 py-2">
-              <FaSort className="inline-block mr-1" />
+            <th className="px-4 py-2 text-start">Payment Mode</th>
+            <th className="px-4 py-2 text-start">
+              <FaSort className="inline-block mr-1 text-start" />
               Status
             </th>
             <th className="px-4 py-2">Action</th>
@@ -45,32 +45,32 @@ export default function Table() {
               <td className="border-gray-300 px-2 py-2 text-center text-black-500">
                 {customerData.trackingId}
               </td>
-              <td className="border-gray-300 px-2 py-2 flex items-center justify-start text-black">
+              <td className="border-gray-300 px-2 py-2 flex items-center justify-start text-black text-start">
                 {customerData.img && (
                   <img
                     src={customerData.img}
                     width={25}
                     height={25}
-                    className="mr-2 rounded-lg"
+                    className="mr-2 rounded-lg text-start"
                     alt="Product"
                   />
                 )}
                 {customerData.productName}
               </td>
 
-              <td className="border-gray-300 px-2 py-2 text-center text-black-500">
+              <td className="border-gray-300 px-2 py-2 text-black-500 text-start">
                 {customerData.customer}
               </td>
-              <td className="border-gray-300 px-2 py-2 text-center text-black-500">
+              <td className="border-gray-300 px-2 py-2 text-black-500 text-start">
                 {customerData.date}
               </td>
-              <td className="border-gray-300 px-2 py-2 text-center text-black-500">
+              <td className="border-gray-300 px-2 py-2 text-black-500 text-start">
                 {customerData.amount}
               </td>
-              <td className="border-gray-300 px-2 py-2 text-center text-black-500">
+              <td className="border-gray-300 px-2 py-2 text-black-500 text-start">
                 {customerData.paymentMode}
               </td>
-              <td className="border-gray-300 px-2 py-2 text-center text-black-500">
+              <td className="border-gray-300 px-2 py-2 text-start text-black-500">
                 <span
                   className={`inline-block px-2 py-1 rounded-2xl ${
                     customerData.status === "Delivered"
